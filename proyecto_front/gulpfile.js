7d067a1f73ea1def29385f0e600e57f0c6bca6b5
Daniel Pinto <danielpinto.ml@gmail.com>
2022-10-27 20:38:08 -0500

    Front, Sass, webp y avif compilado con gulp

---
+++ proyecto_front/gulpfile.js
@@ -0,0 +1,64 @@
+const { src, dest, watch, parallel } = require("gulp");
+
+// css
+const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
+
+// Imágenes
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+
+
+function css( done ) {
+    src('src/scss/**/*.scss')// Identificar el archivo SASS
+        .pipe( plumber() )// No detener la ejecución si hay errores
+        .pipe( sass() )// Compilarlo
+        .pipe( dest("public/build/css") )// Almacenarla en el disco duro
+    done();//Callback que avisa a gulp cuando llegamos al final
+}
+
+function imagenes( done ) {
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe( cache( imagemin(opciones) ))
+        .pipe( dest("public/build/img") )
+    done();
+}
+
+function versionWebp( done ) {
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe( webp(opciones) )
+        .pipe( dest("public/build/img") )
+    done();
+}
+
+function versionAvif( done ) {
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe( avif(opciones) )
+        .pipe( dest("public/build/img") )
+    done();
+}
+
+function dev( done ) {
+    watch("src/scss/**/*.scss", css)//Escuchar la hoja de estilos scss
+    done();//Callback que avisa a gulp cuando llegamos al final
+}
+
+//Ejecutar Invidividualmente
+exports.css = css;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+
+//Ejecutar en conjunto
+exports.dev = parallel(imagenes, versionWebp, versionAvif, dev);
