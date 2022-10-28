3cd7a2a9230f43a19d9c803c13e5ed3805dc12d5
Daniel Pinto <danielpinto.ml@gmail.com>
2022-10-28 15:48:15 -0500

    Front Navbar create

--- proyecto_front/src/App.js
+++ proyecto_front/src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import './build/css/app.css';
 
+
 //Componentes
 import { Navbar } from './components/Navbar';
 
 function App() {
-  console.log(Navbar.tema);
 
   return (
     <div className="App">
       <Navbar />    
-      <div>
-      <h2 data-theme={Navbar.thema}>hola mundo</h2>
-      <h2>hola mundo</h2>
-      <h2>hola mundo</h2>
-      <h2>hola mundo</h2>
-      <h2>hola mundo</h2>
     </div>
-    </div>
-
-    
   );
 }
 
