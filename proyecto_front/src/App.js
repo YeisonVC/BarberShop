7bdd7a7e46a06153927c328b0501de913cc11a5d
Daniel Pinto <danielpinto.ml@gmail.com>
2022-11-06 11:29:17 -0500

    file router done

--- proyecto_front/src/App.js
+++ proyecto_front/src/App.js
@@ -2,18 +2,17 @@ import React from 'react';
 import './build/css/app.css';
 
 //Router v6
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import AppRoutes from './components/router/routes';
 
 //theme
 import { useContext } from 'react';
 import { ThemeContext } from './components/theme/ThemeContext.tsx';
 
-//Componentes
+//Components
 import { Navbar } from './components/navbar/Navbar';
-import { Login }  from './components/login/Login';
-import { Singup } from './components/singup/Singup';
-import { Recuperar } from './components/recuperar/Recuperar';
 import Slider from './components/slider/Slider';
+
+
 
 function App() {
   //Theme
@@ -22,25 +21,18 @@ function App() {
 
   
   return (
-    <BrowserRouter>
-      <div className={`${theme} App`}>
-          <Navbar />
-            <main className='contenedor-app background'>
-                  
-                  <div class="imagen">
-                    <Slider />
-                  </div>
-
-                  <div class="app">
-                    <Routes>
-                      <Route index element={<Login />} />
-                      <Route path='/crear-cuenta' element={<Singup />} />
-                      <Route path='/olvidaste' element={<Recuperar />} />
-                    </Routes>
-                  </div>
-            </main>
+    <div className={`${theme} App`}>
+      <Navbar />
+      <main className='contenedor-app background'>
+        <div class="imagen">
+          <Slider />
         </div>
-    </BrowserRouter>
+
+        <div class="app">
+          <AppRoutes />
+        </div>
+      </main>
+    </div>
   );
 }
 
